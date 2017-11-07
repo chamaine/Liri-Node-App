a47baa6bffd24148414493417e7b4f9bd1bb5761
chamaine <chamaine@wellington-studio.com>
2017-11-06 20:56:25 -0800

    Liri HW2

--- liri.js
+++ liri.js
@@ -21,22 +21,5 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
 
 
-
-
-
-// console.log('Run Test Now');
-// console.log(process.argv);
-// console.log(process.argv[4]);
-// console.log(Array.isArray(process.argv));
-
-// if (process.argv[2]  === 'cats') {
-// console.log('night');
-
-
-// }
-
-// if (process.argv[4] ==='city') {
-// 	console.log('city');
-// }
 // //    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
 
