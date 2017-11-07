56db75e703c8571c4263cdd2193294ad7faeeab1
chamaine <chamaine@wellington-studio.com>
2017-11-07 09:48:13 -0800

    updated Liri3

--- liri.js
+++ liri.js
@@ -1,25 +1,23 @@
 
-var keys = require("...");
+var keys = require("./keys.js");
 var Twitter = require("twitter");
 
 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+var client = new Twitter(keys);
+
+
+
+
+client.get('statuses/user_timeline', {screen_name: 'chamainew'}, function(error, tweets, response) {
+    for (var i = 0; i <tweets.length; i++) {
+        console.log(tweets[i].text);
+
+    }
+
+
+
+ 
 });
-
-
-
-var params = {screen_name: 'chamainew'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-
-
 
 // //    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
 
